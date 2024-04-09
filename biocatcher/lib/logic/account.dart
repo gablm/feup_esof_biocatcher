185b41b456d8df4f588fee38e13f5a94770953cf
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-04-09 18:57:43 +0100

    reformat

--- biocatcher/lib/logic/account.dart
+++ biocatcher/lib/logic/account.dart
@@ -2,77 +2,70 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Account {
- static final Account _instance = Account._privateConstructor();
- static Account get instance => _instance;
- late FirebaseAuth _firebaseAuth;
+  static final Account _instance = Account._privateConstructor();
+  static Account get instance => _instance;
+  late FirebaseAuth _firebaseAuth;
 
- Account._privateConstructor() {
-  _firebaseAuth = FirebaseAuth.instance;
- }
+  Account._privateConstructor() {
+    _firebaseAuth = FirebaseAuth.instance;
+  }
 
- bool isSignedIn() {
-  return _firebaseAuth.currentUser != null;
- }
+  bool isSignedIn() => _firebaseAuth.currentUser != null;
+  User? get currentUser => _firebaseAuth.currentUser;
 
- User? get currentUser => _firebaseAuth.currentUser;
+  Future<void> changePassword(String password) async =>
+      currentUser?.updatePassword(password);
+  Future<void> signOut() async => await _firebaseAuth.signOut();
 
- Future<void> changePassword(String password) async {
-   currentUser?.updatePassword(password);
- }
+  Future<void> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
- Future<void> signOut() async {
-  await _firebaseAuth.signOut();
- }
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
- Future<void> signInWithGoogle() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
 
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    // Once signed in, return the UserCredential
+    await _firebaseAuth.signInWithCredential(credential);
+  }
 
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-   accessToken: googleAuth?.accessToken,
-   idToken: googleAuth?.idToken,
-  );
+  Future<void> signInWithTwitter() async {
+    TwitterAuthProvider twitterProvider = TwitterAuthProvider();
+    await _firebaseAuth.signInWithProvider(twitterProvider);
+  }
 
-  // Once signed in, return the UserCredential
-  await _firebaseAuth.signInWithCredential(credential);
- }
+  Future<void> signInWithEmailAndPassword(
+          String email, String password) async =>
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
 
- Future<void> signInWithTwitter() async {
-  TwitterAuthProvider twitterProvider = TwitterAuthProvider();
-  await _firebaseAuth.signInWithProvider(twitterProvider);
- }
+  Future<void> linkGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
- Future<void> signInWithEmailAndPassword(String email, String password) async {
-  await _firebaseAuth.signInWithEmailAndPassword(
-      email: email,
-      password: password
-  );
- }
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
- Future<void> linkGoogle() async {
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
 
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    currentUser?.linkWithCredential(credential);
+  }
 
-  final credential = GoogleAuthProvider.credential(
-   accessToken: googleAuth?.accessToken,
-   idToken: googleAuth?.idToken,
-  );
+  Future<void> linkTwitter() async {
+    TwitterAuthProvider twitterProvider = TwitterAuthProvider();
+    currentUser?.linkWithProvider(twitterProvider);
+  }
 
-  currentUser?.linkWithCredential(credential);
- }
-
- Future<void> linkTwitter() async {
-  TwitterAuthProvider twitterProvider = TwitterAuthProvider();
-  currentUser?.linkWithProvider(twitterProvider);
- }
-
- Future<void> linkPhone(String phoneNumber) async {
-  currentUser?.linkWithPhoneNumber(phoneNumber);
- }
+  Future<void> linkPhone(String phoneNumber) async {
+    currentUser?.linkWithPhoneNumber(phoneNumber);
+  }
 }
